347711f162cfa1f5a55fe5d70974832db498fd27
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-18 02:34:02 +0100

    adjust padding

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,7 +5,7 @@ import Product from '../components/Product';
 function Shop(id) {
   // console.log('here products', products);
   return (
-    <div className=" w-[70vw] flex p-5 justify-center">
+    <div className=" w-[70vw] flex p-1 lg:p-5 justify-center">
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       {products.map((product) => (
           <div className=" shadow-lg">
