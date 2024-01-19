c293f7e2170bf92a5d96f67a26c5c7cc597db870
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-19 17:31:17 +0100

    Fixing copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' bg-cyan-600 w-screen text-center p-4 text-white font-semibold'>&copy; Duck Shop AB || made by <a className=' cursor-pointer underline hover:text-cyan-950' href="https://github.com/hassanidris" target='_blank'>Hassen Ahmed</a></div>
+    <div className=' bg-cyan-600 w-screen text-center p-4 text-white font-semibold'>&copy; {new Date().getFullYear()} Duck Shop AB || made by <a className=' cursor-pointer underline hover:text-cyan-950' href="https://github.com/hassanidris" target='_blank'>Hassen Ahmed</a></div>
   )
 }
 
